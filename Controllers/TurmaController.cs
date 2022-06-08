d791daeeab2b2e6ce86c06e4058c32b371dc64e1
Fernando Lima <fernando.ilustrar@gmail.com>
2022-06-08 08:10:38 -0300

    Listar Turmas

---
+++ Controllers/TurmaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Curso_Idiomas.Data;
+using System.Collections.Generic;
+using Curso_Idiomas.Models;
+
+namespace Curso_Idiomas.Controllers
+{
+    public class TurmaController : Controller
+    {
+        private readonly CursoIdiomasDbContext _context;
+        public TurmaController(CursoIdiomasDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Turma> turmas = _context.Turma;
+            return View(turmas);
+        }
+    }
+}
