5b17116e454326c6defd2103e225760556f948af
Fernando Lima <fernando.ilustrar@gmail.com>
2022-06-08 08:04:09 -0300

    Listar Disciplinas

---
+++ Controllers/DisciplinaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Curso_Idiomas.Data;
+using System.Collections.Generic;
+using Curso_Idiomas.Models;
+
+namespace Curso_Idiomas.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        private readonly CursoIdiomasDbContext _context;
+        public DisciplinaController(CursoIdiomasDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Disciplina> disciplinas = _context.Disciplina;
+            return View(disciplinas);
+        }
+    }
+}
