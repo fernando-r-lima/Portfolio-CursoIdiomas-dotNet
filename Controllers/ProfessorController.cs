7e48c864e860768badff6aee10047712b5be3ea0
Fernando Lima <fernando.ilustrar@gmail.com>
2022-06-08 07:53:19 -0300

    Listar Professores

---
+++ Controllers/ProfessorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Curso_Idiomas.Data;
+using System.Collections.Generic;
+using Curso_Idiomas.Models;
+
+namespace Curso_Idiomas.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        private readonly CursoIdiomasDbContext _context;
+        public ProfessorController(CursoIdiomasDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Professor> professores = _context.Professor;
+            return View(professores);
+        }
+    }
+}
